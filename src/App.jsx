4dbb942bdc465d66ef409f048f4cf87994f822f6
Hannah Beinstein <hannah.beinstein@gmail.com>
2023-10-05 12:35:29 -0700

    Add inital play functionality

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,41 @@ import viteLogo from "/vite.svg";
 import square from "./assets/1x1.jpg";
 import rectangle from "./assets/3x4.jpg";
 import rectangle2 from "./assets/3x2.jpg";
+import sample from "./assets/sample.mp3";
 import "./App.css";
 import { ImageContainer, Gallery } from "./components";
 
 function App() {
+  const initialSongMap = {
+    currentSong: "",
+    songs: [
+      { name: "Cool Cool Cool", isActive: false },
+      { name: "Apocalypse", isActive: false },
+      { name: "How Sweet Love Is", isActive: false },
+      { name: "Plum Season", isActive: false },
+      { name: "Take Hold Of The Light", isActive: false },
+      { name: "Too Soon", isActive: false },
+      { name: "Vampira", isActive: false },
+      { name: "Young Blood", isActive: false },
+    ],
+  };
+
+  const [songMap, updateSongMap] = useState(initialSongMap);
+
+  const handleClick = (overrides, upcomingSong) => {
+    const prevSong = document.getElementById(`${songMap.currentSong}`);
+    if (prevSong && prevSong.id !== upcomingSong.id) {
+      prevSong.pause();
+    }
+
+    if (!upcomingSong.currentTime || upcomingSong.paused) {
+      upcomingSong.play();
+      updateSongMap({ ...songMap, ...overrides });
+    } else {
+      upcomingSong.pause();
+    }
+  };
+
   return (
     <>
       <Gallery>
@@ -17,6 +48,9 @@ function App() {
           height={100}
           alt={"square image"}
           loading={"eager"}
+          dataSongName={songMap.songs[0].name}
+          dataSongAudio={sample}
+          handleClick={handleClick}
         />
 
         <ImageContainer
@@ -25,6 +59,9 @@ function App() {
           height={100}
           alt={"square image"}
           loading={"eager"}
+          dataSongName={songMap.songs[1].name}
+          dataSongAudio={sample}
+          handleClick={handleClick}
         />
 
         <ImageContainer
@@ -33,6 +70,9 @@ function App() {
           height={300}
           alt={"square image"}
           loading={"eager"}
+          dataSongName={songMap.songs[2].name}
+          dataSongAudio={sample}
+          handleClick={handleClick}
         />
 
         <ImageContainer
@@ -41,6 +81,9 @@ function App() {
           height={300}
           alt={"square image"}
           loading={"eager"}
+          dataSongName={songMap.songs[3].name}
+          dataSongAudio={sample}
+          handleClick={handleClick}
         />
 
         <ImageContainer
@@ -49,6 +92,9 @@ function App() {
           height={300}
           alt={"square image"}
           loading={"eager"}
+          dataSongName={songMap.songs[4].name}
+          dataSongAudio={sample}
+          handleClick={handleClick}
         />
 
         <ImageContainer
@@ -57,6 +103,9 @@ function App() {
           height={300}
           alt={"square image"}
           loading={"eager"}
+          dataSongName={songMap.songs[5].name}
+          dataSongAudio={sample}
+          handleClick={handleClick}
         />
 
         <ImageContainer
@@ -65,6 +114,9 @@ function App() {
           height={300}
           alt={"square image"}
           loading={"eager"}
+          dataSongName={songMap.songs[6].name}
+          dataSongAudio={sample}
+          handleClick={handleClick}
         />
 
         <ImageContainer
@@ -73,6 +125,9 @@ function App() {
           height={300}
           alt={"square image"}
           loading={"eager"}
+          dataSongName={songMap.songs[7].name}
+          dataSongAudio={sample}
+          handleClick={handleClick}
         />
       </Gallery>
     </>
--- src/components/ImageContainer.jsx
+++ src/components/ImageContainer.jsx
@@ -1,12 +1,29 @@
 export function ImageContainer(props) {
+  const formattedSong = props.dataSongName.toLowerCase().replace(/ /gi, "-");
+  const formattedSongId = `${formattedSong}-container`;
   return (
-    <img
-      src={props.src}
-      className={props.className}
-      height={`${props.width}%`}
-      width="auto"
-      alt={props.alt}
-      loading={props.loading}
-    ></img>
+    <div id={formattedSongId} className="gallery__image-container">
+      <button
+        type="button"
+        className="gallery__play-btn"
+        onClick={() => {
+          const container = document.getElementById(formattedSongId);
+          const songAudio = container.querySelector("audio");
+          props.handleClick({ currentSong: props.dataSongName }, songAudio);
+        }}
+      >
+        play
+      </button>
+      <audio id={props.dataSongName} src={props.dataSongAudio}></audio>
+      <img
+        src={props.src}
+        className={props.className}
+        height={`${props.width}%`}
+        width="auto"
+        alt={props.alt}
+        data-song={props.dataSongName}
+        loading={props.loading}
+      ></img>
+    </div>
   );
 }

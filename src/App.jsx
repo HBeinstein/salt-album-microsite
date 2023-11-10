30087a08ec1c589eba3f4319a1cbfdb281904fb0
Hannah Beinstein <hannah.beinstein@gmail.com>
2023-11-09 16:25:48 -0800

    Add hover effect to image grid

--- src/App.jsx
+++ src/App.jsx
@@ -20,9 +20,8 @@ function App() {
 
   useLayoutEffect(() => {
     let ctx = gsap.context(() => {
-      let sections = gsap.utils.toArray(".section");
-
       // Handle overall horizontal scroll
+      let sections = gsap.utils.toArray(".section");
       gsap.to(sections, {
         x: () =>
           -(
@@ -31,7 +30,6 @@ function App() {
           ) + "px",
         ease: "sine",
         scrollTrigger: {
-          markers: true,
           trigger: scrollContainer.current,
           pin: true,
           scrub: 1,
@@ -63,14 +61,135 @@ function App() {
           ease: "sine.in",
           stagger: staggerParams,
         })
-        .from(".gallery__image-container", {
-          opacity: 0,
-          duration: 1,
-          ease: "sine.in",
-          stagger: staggerParams,
-        });
+        .from(
+          ".gallery__image-container",
+          {
+            opacity: 0,
+            duration: 1,
+            ease: "sine.in",
+            stagger: staggerParams,
+          },
+          "<",
+        );
     }, scrollContainer);
     return () => ctx.revert();
+  }, []);
+
+  // Image Specific Animations
+  const [btnText, updateBtnText] = useState("play");
+  const galleryRef = useRef(null);
+
+  // Create Image State Refs for each image
+  const imgState = useRef(null);
+  const imgState2 = useRef(null);
+  const imgState3 = useRef(null);
+  const imgState4 = useRef(null);
+  const imgState5 = useRef(null);
+  const imgState6 = useRef(null);
+  const imgState7 = useRef(null);
+  const imgState8 = useRef(null);
+
+  // Store Image State Refs in an array
+  let imgStateArr = [
+    imgState,
+    imgState2,
+    imgState3,
+    imgState4,
+    imgState5,
+    imgState6,
+    imgState7,
+    imgState8,
+  ];
+
+  function handleMouseEnter(event, index) {
+    imgStateArr[index].current.play();
+
+    if (event.target.dataset.isactive === "true" && btnText != "pause") {
+      updateBtnText("pause");
+    }
+
+    if (event.target.dataset.isactive === "false" && btnText != "play") {
+      updateBtnText("play");
+    }
+    const timeline = gsap.timeline();
+    timeline
+      .to(".inner-cursor__text", {
+        fontSize: 18,
+        duration: 0.175,
+        ease: "sine.in",
+      })
+      .to(
+        ".inner-cursor__background",
+        {
+          width: 80,
+          height: 80,
+          duration: 0.175,
+          ease: "sine.in",
+        },
+        "<",
+      )
+      .to(
+        ".outer-cursor",
+        {
+          opacity: 0,
+          duration: 0.175,
+          ease: "sine.in",
+        },
+        "<",
+      );
+  }
+
+  function handleMouseLeave(event, index) {
+    imgStateArr[index].current.reverse();
+
+    const timeline = gsap.timeline();
+    timeline
+      .to(".inner-cursor__text", {
+        fontSize: 0,
+        duration: 0.175,
+        ease: "sine.in",
+      })
+      .to(
+        ".inner-cursor__background",
+        {
+          width: 6,
+          height: 6,
+          duration: 0.175,
+          ease: "sine.in",
+        },
+        "<",
+      )
+      .to(
+        ".outer-cursor",
+        {
+          opacity: 1,
+          duration: 0.175,
+          ease: "sine.in",
+        },
+        "<",
+      );
+  }
+
+  useLayoutEffect(() => {
+    let imageCtx = gsap.context(() => {
+      const elements = gsap.utils.toArray(
+        galleryRef.current.querySelectorAll(".gallery__image"),
+      );
+
+      // refactor - https://chat.openai.com/c/b70b60ad-2011-40c1-ad9f-2ee7bd64ecbe
+      // Update image state refs
+      elements.forEach((e, i) => {
+        imgStateArr[i].current = gsap.to(e, {
+          duration: 0.15,
+          y: 4,
+          x: -4,
+          scale: 1.025,
+          ease: "sine.in",
+          paused: true,
+        });
+      });
+    }, galleryRef.current);
+    return () => imageCtx.revert();
   }, []);
 
   // Song Logic
@@ -125,9 +244,6 @@ function App() {
   };
 
   // Animations
-
-  const [btnText, updateBtnText] = useState("play");
-  const galleryRef = useRef(null);
 
   let ctx = gsap.context(() => {
     useLayoutEffect(() => {
@@ -193,78 +309,13 @@ function App() {
       });
   }
 
-  function handleMouseEnter(event) {
-    if (event.target.dataset.isactive === "true" && btnText != "pause") {
-      updateBtnText("pause");
-    }
-
-    if (event.target.dataset.isactive === "false" && btnText != "play") {
-      updateBtnText("play");
-    }
-    const timeline = gsap.timeline();
-    timeline
-      .to(".inner-cursor__text", {
-        fontSize: 18,
-        duration: 0.175,
-        ease: "sine.in",
-      })
-      .to(
-        ".inner-cursor__background",
-        {
-          width: 80,
-          height: 80,
-          duration: 0.175,
-          ease: "sine.in",
-        },
-        "<",
-      )
-      .to(
-        ".outer-cursor",
-        {
-          opacity: 0,
-          duration: 0.175,
-          ease: "sine.in",
-        },
-        "<",
-      );
-  }
-
-  function handleMouseLeave() {
-    const timeline = gsap.timeline();
-    timeline
-      .to(".inner-cursor__text", {
-        fontSize: 0,
-        duration: 0.175,
-        ease: "sine.in",
-      })
-      .to(
-        ".inner-cursor__background",
-        {
-          width: 6,
-          height: 6,
-          duration: 0.175,
-          ease: "sine.in",
-        },
-        "<",
-      )
-      .to(
-        ".outer-cursor",
-        {
-          opacity: 1,
-          duration: 0.175,
-          ease: "sine.in",
-        },
-        "<",
-      );
-  }
-
   return (
     <div className="app" ref={app}>
       <div className="scrollContainer" ref={scrollContainer}>
         <div className="section test-component"></div>
         <div className="section test-component2"></div>
         <div className="gallery section" ref={galleryRef}>
-          {songs.map((song) => {
+          {songs.map((song, index) => {
             return (
               <ImageContainer
                 key={song.name}
@@ -279,8 +330,8 @@ function App() {
                 isActive={
                   songMap.songs.find((s) => s.name === song.name).isActive
                 }
-                onEnter={handleMouseEnter}
-                onLeave={handleMouseLeave}
+                onEnter={(event) => handleMouseEnter(event, index)}
+                onLeave={(event) => handleMouseLeave(event, index)}
               ></ImageContainer>
             );
           })}
